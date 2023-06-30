4aaa727f0291429a65f821ac8d9ce46729866772
GaganDang23 <137985599+GaganDang23@users.noreply.github.com>
2023-06-30 11:53:04 +0530

    first commit

---
+++ src/main/java/com/webosmotic/SpringSecurityByJavaConfigApplication.java
@@ -0,0 +1,13 @@
+package com.webosmotic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityByJavaConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityByJavaConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webosmotic/SpringSecurityByJavaConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webosmotic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityByJavaConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
